a237a391f058e834fc7ebf4a768a13717e96a606
aminhl <amin.hlel@esprit.tn>
2023-02-25 14:55:18 +0100

    introducing_jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,24 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('./../models/userModel');
 
 exports.signup = async (req, res) => {
+  const user = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    image: req.body.image,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECURE, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   try {
-    const user = await User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phoneNumber: req.body.phoneNumber,
-      email: req.body.email,
-      image: req.body.image,
-      password: req.body.password,
-      confirmPassword: req.body.confirmPassword,
-    });
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user,
       },
