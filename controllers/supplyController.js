78796964ba6542b0e98bbc6975cc534cd201f44a
aminhl <amin.hlel@esprit.tn>
2023-02-25 21:23:19 +0100

    refactor_supplyController

--- controllers/supplyController.js
+++ controllers/supplyController.js
@@ -1,106 +1,80 @@
 const Supply = require('./../models/supplyModel');
+const AppError = require('./../utils/appError');
 
 exports.addSupply = async (req, res, next) => {
-    const supply = await Supply.create({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        quantity: req.body.quantity,
-        image: req.body.image,
+  const supply = await Supply.create({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    quantity: req.body.quantity,
+    image: req.body.image,
+  });
+  try {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        supply,
+      },
     });
-    try {
-        res.status(201).json({
-        status: 'success',
-        data: {
-            supply,
-        },
-        });
-    } catch (err) {
-        res.status(500).json({
-        status: 'error',
-        message: err,
-        });
-    }
-}
+  } catch (err) {
+    return next(new AppError(err, 500));
+  }
+};
 
 exports.getAllSupplies = async (req, res, next) => {
-    const supplies = await Supply.find();
-    try {
-        res.status(200).json({
-        status: 'success',
-        data: {
-            supplies,
-        },
-        });
-    } catch (err) {
-        res.status(500).json({
-        status: 'error',
-        message: err,
-        });
-    }
-}
+  const supplies = await Supply.find();
+  try {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        supplies,
+      },
+    });
+  } catch (err) {
+    return next(new AppError(err, 500));
+  }
+};
 
 exports.getSupply = async (req, res, next) => {
-    try {
-        const supply = await Supply.findById(req.params.id);
-        if (!supply) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'Supply not found',
-            });
-        }
-        res.status(200).json({
-            status: 'success',
-            data: {
-                supply,
-            },
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err,
-        });
-    }
+  try {
+    const supply = await Supply.findById(req.params.id);
+    if (!supply) return next(new AppError(`Supply not found`, 404));
+    res.status(200).json({
+      status: 'success',
+      data: {
+        supply,
+      },
+    });
+  } catch (err) {
+    return next(new AppError(err, 500));
+  }
 };
 exports.deleteSupply = async (req, res, next) => {
-    try {
-        const supply = await Supply.findByIdAndDelete(req.params.id);
-        if (!supply) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'Supply not found',
-            });
-        }
-        res.status(200).json({
-            status: 'success',
-            message: 'Supply deleted',
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err,
-        });
-    }
-}
+  try {
+    const supply = await Supply.findByIdAndDelete(req.params.id);
+    if (!supply) return next(new AppError(`Supply not found`, 404));
+    res.status(204).json({
+      status: 'success',
+      message: 'Supply deleted',
+    });
+  } catch (err) {
+    return next(new AppError(err, 500));
+  }
+};
 exports.updateSupply = async (req, res, next) => {
-    try {
-        const supply = await Supply.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!supply) {
-            return res.status(404).json({
-                status: 'error',
-                message: 'Supply not found',
-            });
-        }
-        res.status(200).json({
-            status: 'success',
-            data: {
-                supply,
-            },
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err,
-        });
-    }
-}
+  try {
+    const supply = await Supply.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!supply) return next(new AppError(`Supply not found`, 404));
+    res.status(200).json({
+      status: 'success',
+      data: {
+        supply,
+      },
+    });
+  } catch (err) {
+    return next(new AppError(err, 500));
+  }
+};
