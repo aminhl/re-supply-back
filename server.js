affac978c4c52c0ee9d7dada10768031a6da5166
aminhl <amin.hlel@esprit.tn>
2023-02-25 12:03:41 +0100

    resupply_backend_initial_commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`ReSupply Application listening on port ${port}`);
+});
