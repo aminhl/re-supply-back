4e567fd084468b1fe6a66bc2fd61df08b88c7283
aminhl <amin.hlel@esprit.tn>
2023-02-25 13:18:46 +0100

    user_signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,18 @@
 const User = require('./../models/userModel');
 
-exports.signup = async (req, res) => {};
+exports.signup = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: err,
+    });
+  }
+};
