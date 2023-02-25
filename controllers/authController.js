dbee6504862f2c261d99c9663ae65aefd3c7a6e0
aminhl <amin.hlel@esprit.tn>
2023-02-25 14:21:46 +0100

    fixing_a_security_flaw

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,15 @@ const User = require('./../models/userModel');
 
 exports.signup = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+      image: req.body.image,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
     res.status(201).json({
       status: 'success',
       data: {
