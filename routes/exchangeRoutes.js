f3755f25b0296df6d844412b5ee1c2abbcbe4af9
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-03-02 11:57:49 +0100

    getAllRequests

--- routes/exchangeRoutes.js
+++ routes/exchangeRoutes.js
@@ -3,7 +3,7 @@ const exchangeController = require('../controllers/exchangeController');
 const authController = require("../controllers/authController");
 const router = express.Router();
 
-router.route("/").post(authController.protect, authController.restrictTo("admin","member") ,exchangeController.createExchangeRequest);
+router.route("/").post(authController.protect, authController.restrictTo("admin","member") ,exchangeController.createExchangeRequest).get(authController.protect,authController.restrictTo("admin","member"),exchangeController.getAllExchangeRequests);
 router.route("/:id").get(authController.protect, authController.restrictTo("admin","member") ,exchangeController.getExchangeRequestsByUser);
 router.route("/:id").patch(authController.protect, authController.restrictTo("admin","member") ,exchangeController.respondToExchangeRequest);
 
