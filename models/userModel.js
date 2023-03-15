377479e9c1ce4c7bcf0e308f766156372f21771c
Bechir-Marco <bechir.marko@gmail.com>
2023-03-15 04:30:50 +0100

    2FA with sms code

--- models/userModel.js
+++ models/userModel.js
@@ -4,117 +4,121 @@ const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, 'A user must have a first name'],
+    firstName: {
+        type: String,
+        required: [true, 'A user must have a first name'],
 
-  },
-  lastName: {
-    type: String,
-    required: [true, 'A user must have a last name'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'A user must have a last name'],
 
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, 'A user must have a phone number'],
-    default: "none"
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    trim: true,
-    required: [true, 'A user must have an email address'],
-    validate: {
-      validator: function (v) {
-        if (this.provider === 'facebook' && !v) {
-          return false;
-        }
-        return validator.isEmail(v);
-      },
-      message: 'Please provide a valid email address',
     },
-  },
-  images: [String],
-  password: {
-    type: String,
-    minlength: 8,
-    select: false,
-  },
-  confirmPassword: {
-    type: String,
-    validate: {
-      validator: function (previousPassword) {
-        return previousPassword === this.password;
-      },
+    phoneNumber: {
+        type: String,
+        required: [true, 'A user must have a phone number'],
+        default: "none"
     },
-  },
-  role: {
-    type: String,
-    enum: ["admin", "member"],
-    default: "member"
-  },
-  joinedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active:{
-    type: Boolean,
-    default: true,
-    select: false
-  },
-  verified:{
-    type: Boolean,
-    default: false,
-  },
-  emailVerificationToken: String,
-  emailVerificationExpires: Date,
-  facebookId: {
-    type: String,
-    index: { unique: true, sparse: true },
-  },
-  /* more props ll be added later   */
+    email: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: [true, 'A user must have an email address'],
+        validate: {
+            validator: function (v) {
+                if (this.provider === 'facebook' && !v) {
+                    return false;
+                }
+                return validator.isEmail(v);
+            },
+            message: 'Please provide a valid email address',
+        },
+    },
+    images: [String],
+    password: {
+        type: String,
+        minlength: 8,
+        select: false,
+    },
+    confirmPassword: {
+        type: String,
+        validate: {
+            validator: function (previousPassword) {
+                return previousPassword === this.password;
+            },
+        },
+    },
+    role: {
+        type: String,
+        enum: ["admin", "member"],
+        default: "member"
+    },
+    joinedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    emailVerificationToken: String,
+    emailVerificationExpires: Date,
+    facebookId: {
+        type: String,
+        index: {unique: true, sparse: true},
+    },
+    twoFactorAuth: {
+        type: Boolean,
+        default: false
+    }
+    /* more props ll be added later   */
 });
 
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  this.confirmPassword = undefined;
-  next();
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+    next();
 });
 
 userSchema.pre('save', async function (next) {
-  if(!this.isModified('password') || this.isNew) return next();
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
+    if (!this.isModified('password') || this.isNew) return next();
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
 })
 
-userSchema.pre(/^find/, function(next){
-  // This points to the current query
-  this.find({ active: { $ne: false }});
-  next();
+userSchema.pre(/^find/, function (next) {
+    // This points to the current query
+    this.find({active: {$ne: false}});
+    next();
 })
 
 userSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
+    candidatePassword,
+    userPassword
 ) {
-  return await bcrypt.compare(candidatePassword, userPassword);
+    return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp){
-  if(this.passwordChangedAt) return JWTTimestamp < parseInt(this.passwordChangedAt/1000, 10)
-  return false;
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) return JWTTimestamp < parseInt(this.passwordChangedAt / 1000, 10)
+    return false;
 }
 
-userSchema.methods.createPasswordResetToken = function(){
-  const resetToken = crypto.randomBytes(32).toString("hex");
-  this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
-  this.passwordResetExpires = Date.now() + 10*60*1000;
-  return resetToken;
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken;
 }
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,10 @@ const router = express.Router();
 // Authentication Routes
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/login2FA', authController.loginWith2FA);
+
+
+router.post('/twilio/verify', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/checkEmail', authController.checkEmail);
 
@@ -28,9 +32,10 @@ router.route('/auth/facebook/redirect').get(authController.facebookAuthRedirect,
 router.route('/auth/facebook/login').get(authController.facebookLogin);
 router.route('/auth/facebook/login/redirect').get(authController.facebookLoginCallback);
 
+
 // User Routes
 router.patch('/updateProfile', authController.protect, userController.updateProfile);
 router.delete('/deactivateAccount', authController.protect, userController.deactivateAccount);
-router.route('/').get(authController.protect, authController.restrictTo("admin",""),userController.getAllUsers);
+router.route('/').get(authController.protect, authController.restrictTo("admin", ""), userController.getAllUsers);
 
 module.exports = router;
