8451b3e1d0dec5e7ab7a6c3a99f6d724ba4bacf5
aminhl <amin.hlel@esprit.tn>
2023-04-07 15:04:57 +0100

    deleteUserAccount

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,41 +1,75 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const userController = require('../controllers/userController');
-const passport = require('passport');
+const express = require("express");
+const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
+const passport = require("passport");
 
 const router = express.Router();
 
 // Authentication Routes
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/enable2FA', authController.protect, authController.enable2FA);
-router.post('/forgotPassword', authController.forgotPassword);
-router.post('/checkEmail', authController.checkEmail);
-router.get('/email-verification/:id', userController.checkEmailVerification);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/enable2FA", authController.protect, authController.enable2FA);
+router.post("/forgotPassword", authController.forgotPassword);
+router.post("/checkEmail", authController.checkEmail);
+router.get("/email-verification/:id", userController.checkEmailVerification);
 
 // OAuth google routes
-router.get('/auth/google', authController.googleAuth);
-router.get('/auth/google/redirect', authController.googleAuthRedirect, authController.handleGoogleAuth);
-router.get('/auth/google/login', authController.googleLogin);
-router.get('/auth/google/login/redirect', authController.googleLoginCallback);
+router.get("/auth/google", authController.googleAuth);
+router.get(
+  "/auth/google/redirect",
+  authController.googleAuthRedirect,
+  authController.handleGoogleAuth
+);
+router.get("/auth/google/login", authController.googleLogin);
+router.get("/auth/google/login/redirect", authController.googleLoginCallback);
 
 // Email verification-Reset
-router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updatePassword', authController.protect, authController.updatePassword);
-router.get('/verifyEmail/:token', authController.verifyEmail);
+router.patch("/resetPassword/:token", authController.resetPassword);
+router.patch(
+  "/updatePassword",
+  authController.protect,
+  authController.updatePassword
+);
+router.get("/verifyEmail/:token", authController.verifyEmail);
 
 // OAuth facebook routes
-router.route('/auth/facebook').get(authController.facebookAuth);
-router.route('/auth/facebook/redirect').get(authController.facebookAuthRedirect, authController.handleFacebookAuth);
-router.route('/auth/facebook/login').get(authController.facebookLogin);
-router.route('/auth/facebook/login/redirect').get(authController.facebookLoginCallback);
-
+router.route("/auth/facebook").get(authController.facebookAuth);
+router
+  .route("/auth/facebook/redirect")
+  .get(authController.facebookAuthRedirect, authController.handleFacebookAuth);
+router.route("/auth/facebook/login").get(authController.facebookLogin);
+router
+  .route("/auth/facebook/login/redirect")
+  .get(authController.facebookLoginCallback);
 
 // User Routes
-router.patch('/updateProfile', authController.protect, authController.restrictTo("admin", "member"), userController.updateProfile);
-router.delete('/deactivateAccount', authController.protect, userController.deactivateAccount);
-router.get('/user', authController.protect, userController.getUser);
-router.route('/').get(authController.protect, authController.restrictTo("admin", ""), userController.getAllUsers);
-router.route('/update').patch(authController.protect, authController.restrictTo("admin", "member"), authController.updateUser);
+router.patch(
+  "/updateProfile",
+  authController.protect,
+  authController.restrictTo("admin", "member"),
+  userController.updateProfile
+);
+router.delete(
+  "/deactivateAccount",
+  authController.protect,
+  userController.deactivateAccount
+);
+router.get("/user", authController.protect, userController.getUser);
+router
+  .route("/")
+  .get(
+    authController.protect,
+    authController.restrictTo("admin", ""),
+    userController.getAllUsers
+  );
+router
+  .route("/update")
+  .patch(
+    authController.protect,
+    authController.restrictTo("admin", "member"),
+    authController.updateUser
+  );
+
+router.route("/delete/:id").delete(userController.deleteAccount);
 
 module.exports = router;
