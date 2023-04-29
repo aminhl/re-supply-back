762c60e9d5c24be002623ffba319b6cc1df5aa55
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-04-29 17:11:01 +0100

    stripe one product

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,5 +5,5 @@ const router = express.Router();
 
 router.route("/").post(authController.protect, authController.restrictTo("admin") ,orderController.createOrder).get(authController.protect, authController.restrictTo("admin") ,orderController.getAllOrders);
 router.route("/:id").get(authController.protect, authController.restrictTo("admin","member") ,orderController.getOrder).patch(authController.protect, authController.restrictTo("admin","member") ,orderController.updateOrder).delete(authController.protect, authController.restrictTo("admin","member") ,orderController.deleteOrder);
-
+router.route("/product/").post(authController.protect, authController.restrictTo("admin") ,orderController.createSingleOrder);
 module.exports = router;
