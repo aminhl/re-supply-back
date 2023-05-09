51bfb1479ba2e71212c97f586ccf4ffc7ae39aa2
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-05-09 18:18:01 +0100

    category claims

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.route("/").post(authController.protect, authController.restrictTo("admin","member") ,feedbackController.addFeedback).get(authController.protect, authController.restrictTo("admin","member") ,feedbackController.getAllFeedbacks);
 router.route("/:id").delete(authController.protect, authController.restrictTo("admin","member") ,feedbackController.deleteFeedback).patch(authController.protect, authController.restrictTo("admin","member") ,feedbackController.updateFeedback);
 router.route("/get/:id").get(authController.protect, authController.restrictTo("admin","member") ,feedbackController.getFeedback);
+router.route("/positive").get(authController.protect, authController.restrictTo("admin","member") ,feedbackController.getPositiveFeedbacks);
+router.route("/negative").get(authController.protect, authController.restrictTo("admin","member") ,feedbackController.getNegativeFeedbacks);
 module.exports = router;
